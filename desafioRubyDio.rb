c8877ba9b0d8f6e58e0d48a08f3a39bfc15af4e9
Debora <deboralobato40@tutanota.com>
2024-01-28 11:16:53 -0300

    adicionando desafio ruby 01

---
+++ desafioRubyDio.rb
@@ -0,0 +1,13 @@
+print "Digite o seu primeiro nome: "
+
+nome = gets.chomp
+
+print "Digite o seu sobrenome: "
+
+sobrenome = gets.chomp
+
+print "Digite a sua idade: "
+
+idade = gets.to_i
+
+puts "O nome do usuário é #{nome} #{sobrenome} ele(a) tem #{idade} anos!"
